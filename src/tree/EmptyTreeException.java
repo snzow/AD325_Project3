f5a8dd5011e3053f65d27439f099b564d8c6d631
snzow <91493272+snzow@users.noreply.github.com>
2022-11-22 19:00:31 -0800

    first commit

---
+++ src/tree/EmptyTreeException.java
@@ -0,0 +1,14 @@
+package tree;
+
+public class EmptyTreeException extends RuntimeException
+{
+    public EmptyTreeException()
+    {
+        this(null);
+    } // end default constructor
+
+    public EmptyTreeException(String message)
+    {
+        super(message);
+    } // end constructor
+} // end EmptyTreeException
